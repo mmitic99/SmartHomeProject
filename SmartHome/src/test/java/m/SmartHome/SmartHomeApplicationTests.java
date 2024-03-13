c6141ce66ab3252af321faad6299aa500b6b8dbe
Mihajlo Mitic <mihajlomiticmiki@gmail.com>
2024-03-13 18:02:45 +0100

    init SmartHome backend

---
+++ SmartHome/src/test/java/m/SmartHome/SmartHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package m.SmartHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
