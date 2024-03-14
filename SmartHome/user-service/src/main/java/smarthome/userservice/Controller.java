20b54641065cacd1ef381e6dffad205eed23ef16
Mihajlo Mitic <mihajlomiticmiki@gmail.com>
2024-03-14 15:49:28 +0100

    added and connected discovery-service, user-service, and api-gateway

---
+++ SmartHome/user-service/src/main/java/smarthome/userservice/Controller.java
@@ -0,0 +1,16 @@
+package smarthome.userservice;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+public class Controller {
+    @GetMapping("")
+    public ResponseEntity<String> check(){
+        return ResponseEntity.ok("WORK");
+    }
+}
